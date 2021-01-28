a777544ba31629dd6c2b0d4a0a273f6ec044d3c3
Kirill Matskevich <inflamedbrain@gmail.com>
2021-01-28 11:18:39 +0200

    fixed SPM file;

--- Package.swift
+++ Package.swift
@@ -16,6 +16,6 @@ let package = Package(
     targets: [
         .target(
             name: "QLog",
-            dependencies: [Cuckoo, Zip]),
+            dependencies: ["Cuckoo", "Zip"]),
     ]
 )
